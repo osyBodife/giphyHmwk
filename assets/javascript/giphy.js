cde215569619da9585a32890a12da22d6f926103
Osy odife <osyodife@gmail.com>
2019-07-11 13:19:39 -0400

    upload modified .js files

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -2,23 +2,19 @@
 // create an array of few animals to display as home page
 //this would display before any user input
 var animalArray = ["cat", "dog", "horse", "monkey", "goat"];
-//create a variable to hold button id 
-//create function to display initial animal list
-//create variable for all queries
-
+//create function to create buttons
+//display the button
 function createBtns() {
     for (var i = 0; i < animalArray.length; i++) {
-        // clear existing button to avoid dupilcate
-        //$("#imageBtns").empty();
-        //create a Button
+       //create a Button
         var aniBtn = $("<button>");
-
-        //add attributes to the btn
+        //add attributes to the button just created
+        //btnClass is defined in .css file
         aniBtn.addClass("btnClass");
+        //data-type is a data* attribute, provides the name the of button
         aniBtn.attr("data-type", animalArray[i]);
-        aniBtn.text(animalArray[i]);
-
-        aniBtn.text(animalArray[i]);
+        //write the name of each button
+        aniBtn.text(animalArray[i]);      
         //display button on home page            
         $("#imageBtns").append(aniBtn);
 
@@ -27,22 +23,27 @@ function createBtns() {
 
 
 
-//create a new button on form submit
+//create a function to create new button on User input and form submission
 $("#submitBtn").on("click", function (event) {
+    //prevent form from deleting html element after submission
     event.preventDefault();
+    //get user input and covert to lower case letters
     var userText = $("#search").val().trim().toLowerCase();
+    //prevent user from submitting empty field
     if (userText == "") {
         return false;
     } else {
-        //add to original array
+        //add User Input to original array
         //check if the name exists
+        //prevent duplication of buttons
         if(animalArray.includes(userText)==true){
             alert("Error! the name alreay exists");
         }else{
             animalArray.push(userText);
-        // clear existing button to avoid dupilcate
+        // clear existing buttons to avoid dupilcates
         $("#imageBtns").empty();
-        //call function to create button
+        //call function to create buttons
+        //will create a new set of buttons
         createBtns();
 
         }
@@ -51,8 +52,7 @@ $("#submitBtn").on("click", function (event) {
     }
 
 });
-//creatr function to get gipghy api
-
+//create a function to get gipghy api
 $(document).on("click", ".btnClass", function () {
 
     $("#giphyImages").empty();
@@ -61,52 +61,46 @@ $(document).on("click", ".btnClass", function () {
     //make the current image active
     $(this).addClass("active");
     //get the value of button from the data attribute
-    //assign it to variable
+    //assign it to a variable
     let searchTerm = $(this).attr("data-type");
-
     let queryURL = "http://api.giphy.com/v1/gifs/search?api_key=lISwnQ5TdGTfXd9Ex4N17L98lbq6KRRi&limit=10&q=" + searchTerm;
+    //make an ajax call
     $.ajax({
         url: queryURL,
         method: "GET"
     })// We store all of the retrieved data inside of an object called "response"
         .then(function (response) {
-            console.log(response);
+            //console.log(response);
             //store all data from ajax call to a variable
             let results = response.data;
-            //consider putting an alert for empty search
+            //alert the user when there no images for current search form giphy.com
             if(results.length===0){
                 alert("Sorry there is NO giphy images for your current search")
             }else{
             //loop thru entire results
             for (var i = 0; i < results.length; i++) {
-                // create a div where to container each image
-                let imgDiv = $("<div class=\"imgItem\">");
+                // create a div where to container each image               
+                let imgDiv=$("<div>");
+                //add class to the image Div
+                imgDiv.addClass("imgItem");
                 //get each image rating
                 let imageRating = results[i].rating;
                 //create p element to house ratings
                 let p= $("<p>").text("Ratings: " + imageRating);
                 //assign p tag attributes
                 p.addClass("ratingClass");
-                /*
-                let animated = results[i].images.fixed_height.url;
-                let still = results[i].images.fixed_height.url;
-                //create the image tag
+               
+                //create the image tag to house src
                 let animalImage = $("<img>");
-                //assign attributes to the image tag
-                animalImage.attr("src", still);
-                animalImage.attr("data-still", still);
-                animalImage.attr("data-image", animated);
-                animalImage.attr("data-state", still);
-                */
-                //create the image tag
-                let animalImage = $("<img>");
+                //assign image src to the results
 				animalImage.attr("src", results[i].images.fixed_height_small_still.url);
 				//pause images
 				animalImage.attr("data-still", results[i].images.fixed_height_small_still.url);
 				//animated images
 			    animalImage.attr("data-animate", results[i].images.fixed_height_small.url);
 				//how images come in, already paused
-			    animalImage.attr("data-state", "still");                
+                animalImage.attr("data-state", "still"); 
+                //add class to use for event Listerner               
                 animalImage.addClass("animal-image");
                 //append rating unto the div
                 imgDiv.append(p);
@@ -127,9 +121,9 @@ $(document).on("click", ".btnClass", function () {
 });
 
 
-    //set the state from still to animated when clickling on images
+   //create a function toggle btw still and animate
     $(document).on("click",".animal-image", function() {
-        var state = $(this).attr('data-state');
+        let state = $(this).attr('data-state');
         if (state == 'still') {
             $(this).attr('src', $(this).data('animate'));
             $(this).attr('data-state', 'animate');
